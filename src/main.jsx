afddd9c585becc2659a90c4b6824e5618ac6dcf6
Zohaib Farooq <mzohaib13d@gmail.com>
2025-10-15 05:58:40 +0500

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 // Direct implementation in main.jsx
-// document.addEventListener("contextmenu", e => e.preventDefault());
+document.addEventListener("contextmenu", e => e.preventDefault());
 document.addEventListener("keydown", e => {
   const key = e.key.toLowerCase();
   if (
